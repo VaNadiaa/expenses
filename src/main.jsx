a038f10edcd5b3870e1582b955ae72be24fe5cb2
VaNadiaa <nadia.vasilevskayaa@gmail.com>
2024-10-17 11:20:53 +0300

    adding history

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,19 @@ import store, { persistor } from "./app/store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { PersistGate } from "redux-persist/integration/react";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
+
+const history = createBrowserHistory();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <Router history={history} basename="/expenses">
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
